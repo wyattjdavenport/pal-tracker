29fc5fdb38a14cedbb42898ad8221e58cc13e43c
Ubuntu <ubuntu@ip-172-31-80-189.ec2.internal>
2020-12-17 16:21:38 +0000

    jdbc add

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,8 +1,11 @@
 package io.pivotal.pal.tracker;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -12,8 +15,9 @@ public class PalTrackerApplication {
     }
 
     @Bean
-    public TimeEntryRepository timeEntryRepository(){
-        return new InMemoryTimeEntryRepository();
+    public JdbcTimeEntryRepository timeEntryRepository(DataSource dataSource) {
+
+        return new JdbcTimeEntryRepository(dataSource);
     }
 
 }
